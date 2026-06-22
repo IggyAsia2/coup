f6f80b70b3341784e7fc4bea01669ad93a763cb7
IggyAsia2 <luongxuanbao@gmail.com>
2026-06-22 13:14:15 +0700

    'update'

--- server/src/index.js
+++ server/src/index.js
@@ -8,7 +8,7 @@ const app = express();
 const httpServer = createServer(app);
 
 // Serve React build trong production
-const publicDir = path.join(__dirname, '../../public');
+const publicDir = path.join(__dirname, '../public');
 app.use(express.static(publicDir));
 app.get('*', (req, res) => {
   res.sendFile(path.join(publicDir, 'index.html'));
